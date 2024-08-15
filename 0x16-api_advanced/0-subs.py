76b39389b8ce78be8ce620fef4810d0d3e075651
CyrusMusyoka <vicrazecyrus@gmail.com>
2024-08-15 08:25:18 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"subscribers count"
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "python:subreddit.subscriber.count:v1.0 (by /u/Even-Lingonberry-795)"}
+    
     response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
-        return 0
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
     else:
-        return data['data']['subscribers']
+        return 0
+
